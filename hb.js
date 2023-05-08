b51bfd7f7032fcead9118e7573611c2a36f635bb
Aigli <aigli.chroni@MacBook-Air-Aigle.local>
2023-05-08 14:57:43 +0300

    happy birthday

---
+++ hb.js
@@ -0,0 +1,25 @@
+const number = document.querySelector(".number");
+const valueNumber = Number(number.getAttribute("data-value"));
+
+let counter = 0;
+const myTimeout = setTimeout(myGreeting, 5000);
+
+function myGreeting() {
+    console.log("came")
+    var hiddenElements = document.querySelectorAll(".visually-hidden");
+    hiddenElements.forEach(element => {
+        element.classList.remove("visually-hidden");
+    });
+  clearTimeout(myTimeout);
+}
+
+const time = () => {
+	setInterval(() => {
+		if (counter !== valueNumber) {
+			counter++;
+			number.innerHTML = `${counter}%`;
+		}
+	}, 50);
+};
+
+time();
